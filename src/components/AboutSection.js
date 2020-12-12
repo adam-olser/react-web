a729b4ade928c0935fa956fd0c56f598aa1cd5d3
adam.olser <adam.olser@kiwi.com>
2020-12-12 18:59:32 +0100

    init commit

---
+++ src/components/AboutSection.js
@@ -0,0 +1,32 @@
+import home1 from '../img/home1.png'
+
+const AboutSection = () => {
+  return(
+      <div>
+        <div className="description">
+          <div className="title">
+            <div className="hide">
+              <h2>
+              We work to make
+              </h2>
+            </div>
+            <div className="hide">
+              <h2>
+                your <span>dreams</span>
+              </h2>
+            </div>
+            <div className="hide">
+              <h2>
+                come true.
+              </h2>
+            </div>
+            <p>Contact us for any photography or video ideas. We have professionals with amazing skills</p>
+            <button>Contact us</button>
+          </div>
+          <img src={home1} alt="guy with a camera"/>
+        </div>
+      </div>
+  )
+}
+
+export default AboutSection
